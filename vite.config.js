f4731d0e85ceda192906e42dd1ce24e11205c011
samueladegnan <samueladegnan@gmail.com>
2026-07-08 12:23:39 -0500

    refactor: flatten repo structure to root

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  base: '/SamuelADegnan/',
+  plugins: [react(), tailwindcss()],
 })
